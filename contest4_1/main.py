fca365eba494d37253f58a9c56780abb88c7f6f0
Ngo Hoang Nhat Quang <113534334+OnesNT@users.noreply.github.com>
2022-11-05 05:15:54 +0300

    Update main.py

--- contest4_1/main.py
+++ contest4_1/main.py
@@ -1,25 +1,3 @@
-# def fact(n):
-#     temp = 1
-#     for i in range (1, n + 1):
-#         temp *= i
-#     return temp
-#
-#
-# def combination(n,  k):
-#     return int(fact(n)) // int(fact(k) * fact(n - k)) ;
-#
-# def pascal_triangle() :
-#     n1 = 0
-#     while True:
-#     # for n1 in range(x):
-#         k1 = 0
-#         while k1 <= n1:
-#             print(combination(n1, k1), end = ' ')
-#             k1 += 1
-#         n1 += 1
-#
-# pascal_triangle()
-
 def pascal_triangle():
     A = [1]
     while True:
