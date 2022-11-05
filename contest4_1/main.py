fee4f9f787e7a3263576e9273afe2e2d2a9e45fb
Ngo Hoang Nhat Quang <113534334+OnesNT@users.noreply.github.com>
2022-11-05 05:16:16 +0300

    Update main.py

--- contest4_1/main.py
+++ contest4_1/main.py
@@ -7,12 +7,6 @@ def pascal_triangle():
         for i in range(1, len(A)):
             B.append(A[i - 1] + A[i])
         B.append(1)
-        # print(B)
         A = B.copy()
-# gen = pascal_triangle()
-#
-# for value in gen:
-#     if value > 1000:
-#         break
-#     print(value, end = ' ')
+
 
